861016f8dfe82d9f5d7eb6fe7ef8be4a5b84c44b
arachoxs <j.agudelo9@utp.edu.co>
2025-01-27 10:36:20 -0500

    adding media querys

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const textMessage = "This Field is required";
 const emailMessage = "Please enter a valid email addres";
 const radioMessage = "please select a query type";
 const checkboxMessage = "To submit this form, please consent to being contacted";
+let validForm
 
 
 function messageAdder(div,message){
+    validForm=false;
     div.innerHTML+=`
     <div class="errorMessage" >
         <p>${message}</p>
@@ -16,11 +18,12 @@ function messageAdder(div,message){
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    validForm=true;
     const divElements= document.querySelectorAll(".form-group,#radioPair,.row-group");
     console.log(divElements);
     for(div of divElements){
         let input = div.querySelector("input") || div.querySelector("textarea");
-
+        console.log(div);
         if(input && !input.validity.valid){
             if(!div.querySelector(".errorMessage")){
                 if(input.type === "text" || input.type === "textarea"){
@@ -32,12 +35,21 @@ form.addEventListener("submit", (event) => {
                 }else if(input.type === "checkbox"){
                     messageAdder(div,checkboxMessage);
                 }
+
+                if(input.type !== "radio"){
+                    div.classList.add("red-border");
+                }
+
             }
+            
         }else{
             const element = div.querySelector(".errorMessage");
             if(element){
                 element.remove();
+                div.classList.remove("red-border"); 
             }
         }
     }
+
+    
 })
